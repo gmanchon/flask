c7a7172047dba5d7eb42d2bf96a1b3ca2966ed07
Gaetan Manchon <gmanchon@gmail.com>
2020-08-21 01:16:52 +0200

    ifmain added in order to start the server using python

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ You may run this on your machine:
 ``` bash
 env FLASK_APP=app.py flask run
 ```
+
+Or through the `ifmain`:
+
+``` bash
+python app.py
+```
--- app.py
+++ app.py
@@ -8,3 +8,6 @@ def hello():
     # get param from http://127.0.0.1:5000/?name=value
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
