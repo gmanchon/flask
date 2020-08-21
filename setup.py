47e639bd4ae4d4427927cc930d73d50ee3222a4f
Gaetan Manchon <gmanchon@gmail.com>
2020-08-21 02:17:22 +0200

    heroku conf added

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+    'Flask==1.1.1',
+    'Flask-Cors==3.0.8',
+    'numpy==1.18.4',
+    'pandas==0.24.2',
+    'joblib==0.14.1',
+    'scikit-learn==0.20.4',
+    'scipy==1.2.2',
+    'xgboost==1.1.1',
+    'mlflow==1.8.0',
+    'google-cloud-storage==1.26.0',
+    'gcsfs==0.6.0',
+    'pygeohash',
+    'category_encoders',
+    'psutil==5.7.0',
+    'pygeohash==1.2.0',
+    'termcolor==1.1.0',
+    'memoized-property==1.0.3',
+    'category_encoders==2.2.2']
+
+setup(
+    name='TaxiFareModelAPI',
+    version='1.0',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='taxi fare model api'
+)
